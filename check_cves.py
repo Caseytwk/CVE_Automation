d98a7d5c58513159ac4fead89661842add2c2617
Caseytwk <casey.twk.02@gmail.com>
2025-06-27 11:51:51 +0800

    Update check_cves.py

--- check_cves.py
+++ check_cves.py
@@ -6,7 +6,7 @@ from datetime import datetime, timezone
 KEYWORDS = [
     {"sdk": "RTL8720CM", "search": "RTL8720CM"},
     {"sdk": "Ameba SDK", "search": "Ameba"},
-    {"sdk": "RTL8762EKF", "search": "RTL8762EKF"},
+    #{"sdk": "RTL8762EKF", "search": "RTL8762EKF"},
 ]
 
 headers = {"User-Agent": "cve-monitor/1.0"}
